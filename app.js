bb749337e1a15e3fa74ea6f00e73466fcff2b0b5
WangariNelly <nellywangari30@gmail.com>
2024-10-04 22:07:40 +0300

    Swagger Documentation

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 require('dotenv').config({ path: '/config/.env' });
+
 const express = require('express');
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+
 const usersRoute = require('./routes/authRoute.js');
 const db = require('./db/db.js');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const errorMiddleware = require('./middlewares/errors');
+const swaggerDefinition = require('./swagger.json');
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,8 +19,15 @@ app.use((req, res, next) => {
   req.db = db; // Add db to request object
   next(); // Move to the next middleware/route handler
 });
+const baseUrl = '/api/v1';
 
-app.use('/api/v1', usersRoute);
+app.use(baseUrl, usersRoute);
+
+app.use(
+  baseUrl + '/swagger',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDefinition),
+);
 
 // app.get('/users', async (req, res) => {
 //   try {
@@ -26,5 +38,6 @@ app.use('/api/v1', usersRoute);
 //       res.status(500).json({ error: 'Internal Server Error' });
 //   }
 // });
+app.use(errorMiddleware);
 
 module.exports = app;
