7bd775bb5a3b9958f823d57ee78e39481b94dcfc
WangariNelly <nellywangari30@gmail.com>
2024-11-04 12:23:50 +0300

    changes

--- todo-frontend/src/app/app.routes.ts
+++ todo-frontend/src/app/app.routes.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { RouterModule,Routes } from '@angular/router';
+
+import { Routes } from '@angular/router';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { TodoFormComponent } from './components/todo-form/todo-form.component';
 import { TodoDetailComponent } from './components/todo-detail/todo-detail.component';
@@ -18,9 +18,3 @@ export const routes: Routes = [
   { path: 'todos/:id', component: TodoDetailComponent },
   { path: '', redirectTo: '/todos', pathMatch: 'full' },
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule {}
